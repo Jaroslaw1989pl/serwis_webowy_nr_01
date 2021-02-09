e73c1768abc63d6857176d7518dbacda9dd647bc
Jarek <jarekpoczta1989@wp.pl>
2021-02-09 18:46:32 +0100

    wtorek 09 luty 2021

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+    <title>Swerwis Webowy nr 01</title>
+
+    <link rel="stylesheet" href="index_style.css">
+</head>
+<body>
+    <main>
+        <header>
+            <div id="top_bar">
+                <input type="button" id="login_form_button" value="ZALOGUJ SIĘ">
+                <input type="button" id="register_form_button" value="ZAREJESTRUJ SIĘ">
+            </div>
+        </header>
+    </main>
+</body>
+</html>
